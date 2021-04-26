81c58bae160cdcc251017585132125faa19cad74
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-04-26 16:41:33 +0300

    modify: added client_max_body_size to nginx.sh

--- files/nginx.sh
+++ files/nginx.sh
@@ -11,6 +11,12 @@ if ! grep -q "worker_rlimit_nofile    20000;" /usr/local/etc/nginx/nginx.conf; t
   gsed -i '/worker_processes  auto;/a worker_rlimit_nofile    20000;' /usr/local/etc/nginx/nginx.conf
 fi
 
+echo "Set client_max_body_size to 512M"
+
+if ! grep -q "client_max_body_size 512M;" /usr/local/etc/nginx/nginx.conf; then
+  gsed -i 'http {/a client_max_body_size 512M;' /usr/local/etc/nginx/nginx.conf
+fi
+
 echo "Create nginx ssl directory if doesn't exist"
 
 mkdir -p /usr/local/etc/nginx/ssl
