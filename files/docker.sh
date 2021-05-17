99a11e67397090a85c770dab27aa90772b46db1d
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-05-17 21:47:31 +0300

    modify: changed deploy script, added tagger

--- files/docker.sh
+++ files/docker.sh
@@ -11,6 +11,19 @@ if [[ -n $(sed -n -e "s/^DOCKER_APP_NAME=//p" .env ) ]]; then
   DOCKER_APP_NAME=$(sed -n -e "s/^DOCKER_APP_NAME=//p" .env )
 fi
 
+#temporary fix for mysql
+maxcounter=45
+counter=1
+while ! docker-compose exec -T mysql mysql -h127.0.0.1 \
+  -uroot -proot -e "show databases;" > /dev/null 2>&1; do
+  sleep 1
+  counter=$(( "$counter" + 1 ))
+  if [ $counter -gt $maxcounter ]; then
+    >&2 echo "We have been waiting for MySQL too long already; failing."
+    exit 1
+  fi;
+done
+
 docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
   -e "GRANT ALL PRIVILEGES ON *.* TO 'tenantcloud'@'%';"
 docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
