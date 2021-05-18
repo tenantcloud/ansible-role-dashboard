f5342b2e677ebcce26d55db466b450dc931bc9ad
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-05-18 16:22:37 +0300

    modify: added DOCKER_APP_NAME, DOCKER_MYSQL_NAME to deploy script

--- files/docker.sh
+++ files/docker.sh
@@ -6,15 +6,13 @@
 cd "$(dirname "$0")"/../../ || exit
 PATH=$(pwd)/cli:$PATH
 
-#get DOCKER_APP_NAME from .env
-if [[ -n $(sed -n -e "s/^DOCKER_APP_NAME=//p" .env ) ]]; then
-  DOCKER_APP_NAME=$(sed -n -e "s/^DOCKER_APP_NAME=//p" .env )
-fi
+DOCKER_APP_NAME="app"
+DOCKER_MYSQL_NAME="mysql"
 
 #temporary fix for mysql
 maxcounter=45
 counter=1
-while ! docker-compose exec -T mysql mysql -h127.0.0.1 \
+while ! docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 \
   -uroot -proot -e "show databases;" > /dev/null 2>&1; do
   sleep 1
   counter=$(( "$counter" + 1 ))
@@ -24,23 +22,23 @@ while ! docker-compose exec -T mysql mysql -h127.0.0.1 \
   fi;
 done
 
-docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
+docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 -uroot -proot \
   -e "GRANT ALL PRIVILEGES ON *.* TO 'tenantcloud'@'%';"
-docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
+docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 -uroot -proot \
   -e "FLUSH PRIVILEGES;"
-docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
+docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 -uroot -proot \
   -e "CREATE USER 'read'@'%' IDENTIFIED BY 'read';"
-docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
+docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 -uroot -proot \
   -e "GRANT SELECT, SHOW VIEW ON *.* TO 'read'@'%';"
-docker-compose exec -T mysql mysql -h127.0.0.1 -uroot -proot \
+docker-compose exec -T "${DOCKER_MYSQL_NAME}" mysql -h127.0.0.1 -uroot -proot \
   -e "FLUSH PRIVILEGES;"
-docker-compose exec -T app composer install --no-interaction --prefer-dist --optimize-autoloader
-docker run --rm -i --network=tc-"${DOCKER_APP_NAME:-app}"-network --mount source=tc-"${DOCKER_APP_NAME:-app}"-nfsmount,target=/app \
+docker-compose exec -T "${DOCKER_APP_NAME}" composer install --no-interaction --prefer-dist --optimize-autoloader
+docker run --rm -i --network=tc-"${DOCKER_APP_NAME}"-network --mount source=tc-"${DOCKER_APP_NAME}"-nfsmount,target=/app \
   -w /app --entrypoint "/app/sh/install/minio.sh"  minio/mc:latest
-docker-compose exec -T app php artisan passport:keys
-docker-compose exec -T app php artisan migrate
-docker-compose exec -T app php artisan db:seed
-docker-compose exec -T app php artisan tc:devops:passport_install --force
+docker-compose exec -T "${DOCKER_APP_NAME}" php artisan passport:keys
+docker-compose exec -T "${DOCKER_APP_NAME}" php artisan migrate
+docker-compose exec -T "${DOCKER_APP_NAME}" php artisan db:seed
+docker-compose exec -T "${DOCKER_APP_NAME}" php artisan tc:devops:passport_install --force
 $(command -v yarn) ci
 $(command -v yarn) release
-docker-compose exec -T app ln -s /app/resources/web /app/public/web
+docker-compose exec -T "${DOCKER_APP_NAME}" ln -s /app/resources/web /app/public/web
